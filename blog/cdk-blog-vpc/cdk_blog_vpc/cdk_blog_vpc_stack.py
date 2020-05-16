311a768966c9fa4f5d47814e891fc896bb900afe
EC2 Default User <ec2-user@ip-172-31-90-108.ec2.internal>
2020-05-16 19:50:17 +0000

    blog

--- blog/cdk-blog-vpc/cdk_blog_vpc/cdk_blog_vpc_stack.py
+++ blog/cdk-blog-vpc/cdk_blog_vpc/cdk_blog_vpc_stack.py
@@ -11,15 +11,13 @@ default_vpc_cidr_range="10.0.0.0/16"
         
 
 class VPCPeeringConnection(ec2.CfnVPCPeeringConnection):
-    def __init__(self,scope, id,  vpc,peer_vpc,  **kwargs):
+    def __init__(self,scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
         
 
 class CdkBlogVpcStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, vpc_name:str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-            
+    def get_next_cidr_range(self,vpc_name,**kwargs):
         # The code that defines your stack goes here
         dynamodb = boto3.resource('dynamodb', region_name=kwargs['env'].region)
 
@@ -42,8 +40,29 @@ class CdkBlogVpcStack(core.Stack):
             last_cidr_range=response['Item']['value']
             next_cidr_range=increment_cidr_range(last_cidr_range);
 
+        cidr_range_table = dynamodb.Table('cidr_range_table')
+        response_cidr_range_table = cidr_range_table.put_item(
+           Item={
+                'id': vpc_name,
+                'vpc': next_cidr_range
+            }
+        )
+        
+        response_last_cidr_range_table = last_cidr_range_table.put_item(
+           Item={
+                'id': 'last_cidr_range',
+                'value': next_cidr_range
+            }
+        )
+
         print('NEXT CIDR:')
         print(next_cidr_range)
+        return next_cidr_range
+
+    def __init__(self, scope: core.Construct, id: str, vpc_name:str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+            
+        next_cidr_range = self.get_next_cidr_range(vpc_name,**kwargs)
         #Provisioning VPC
         self.vpc = ec2.Vpc(self, vpc_name,
             cidr=next_cidr_range,
@@ -68,27 +87,13 @@ class CdkBlogVpcStack(core.Stack):
         
         
         
-        cidr_range_table = dynamodb.Table('cidr_range_table')
-        response_cidr_range_table = cidr_range_table.put_item(
-           Item={
-                'id': vpc_name,
-                'vpc': next_cidr_range
-            }
-        )
         
-        response_last_cidr_range_table = last_cidr_range_table.put_item(
-           Item={
-                'id': 'last_cidr_range',
-                'value': next_cidr_range
-            }
-        )
         
         
 class CdkBlogVpcPeeringStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, vpc:ec2.Vpc,peer_vpc:ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        print(vpc)
-        vpc_peer=VPCPeeringConnection(self,'test', vpc.vpc_id, peer_vpc.vpc_id)
+        vpc_peer=VPCPeeringConnection(self,'test', vpc_id=vpc.vpc_id, peer_vpc_id=peer_vpc.vpc_id)
         
         
 def increment_cidr_range(current_cidr_range):
